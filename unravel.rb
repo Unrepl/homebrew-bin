e5fe61ea14853f2613610415e50e68c6d8ea495d
Paulus Esterhazy <pesterhazy@gmail.com>
2017-12-31 17:43:04 +0100

    Add `clojure` as dependency

--- unravel.rb
+++ unravel.rb
@@ -3,9 +3,11 @@ class Unravel < Formula
   homepage "https://github.com/pesterhazy/unravel"
   url "https://github.com/Unrepl/unravel/releases/download/v0.3.0-beta.2/unravel.zip"
   sha256 "093b7b4d720afc3b482e6599e13103739ebaf48e8267f0a79d2703b306651777"
-  version "0.3.0-beta.2"
+  version "0.3.0-beta.2-1"
 
   bottle :unneeded
+
+  depends_on "clojure"
 
   def install
     prefix.install Dir["*"]
